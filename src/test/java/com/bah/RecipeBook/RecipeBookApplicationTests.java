e8e588721cd5e9ca0e1d49cb5a7e63c18e2d7579
Sam Gordon <gordon_samuel@bah.com>
2022-07-06 12:22:25 -0500

    Initial Commit

---
+++ src/test/java/com/bah/RecipeBook/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.RecipeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
