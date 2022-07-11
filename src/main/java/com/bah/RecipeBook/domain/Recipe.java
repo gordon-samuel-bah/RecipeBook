188fb1b6b9ad2d3ede909e10f987632d3c13e453
Sam Gordon <gordon_samuel@bah.com>
2022-07-11 09:16:50 -0500

    Added tests and other functionality

--- src/main/java/com/bah/RecipeBook/domain/Recipe.java
+++ src/main/java/com/bah/RecipeBook/domain/Recipe.java
@@ -40,6 +40,9 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    public Recipe() {
+    }
+
     public void setNotes(Notes notes) {
         this.notes = notes;
         notes.setRecipe(this);
---
+++ src/test/java/com/bah/RecipeBook/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.bah.RecipeBook.services;
+
+import com.bah.RecipeBook.domain.Recipe;
+import com.bah.RecipeBook.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    RecipeServiceImpl recipeService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
