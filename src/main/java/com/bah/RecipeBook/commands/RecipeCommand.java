4890a203a49e4e2a5ac63478b2af67d1efbe7f0d
Sam Gordon <gordon_samuel@bah.com>
2022-07-25 17:09:44 -0500

    Add MySQL connection.

--- src/main/java/com/bah/RecipeBook/commands/RecipeCommand.java
+++ src/main/java/com/bah/RecipeBook/commands/RecipeCommand.java
@@ -4,7 +4,9 @@ import com.bah.RecipeBook.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,12 +15,29 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(999)
     private Integer servings;
     private String source;
+
+    @URL
+    @NotBlank
     private String url;
+
+    @NotBlank
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
